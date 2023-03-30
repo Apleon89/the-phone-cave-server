c40328a2c1cb495f5616e9eb0f8e3f9bef577863
Alberto Pérez <apleon89@gmail.com>
2023-03-30 15:49:52 +0100

    prettier

--- routes/phone.routes.js
+++ routes/phone.routes.js
@@ -3,21 +3,21 @@ const Phones = require("../models/Phone.model");
 
 // Get all Phones
 router.get("/", async (req, res, next) => {
-    try {
-        const response = await Phones.find().select('id name')
-        res.json(response);
-    } catch (error) {
-        next(error)
-    }
-  });
+  try {
+    const response = await Phones.find().select("id name");
+    res.json(response);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.get('/:id', async (req, res, next) => {
-    const { id } = req.params
-    try {
-        const response = await Phones.findById(id)
-        res.json(response)
-    } catch (error) {
-        next(error)
-    }
-})
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const response = await Phones.findById(id);
+    res.json(response);
+  } catch (error) {
+    next(error);
+  }
+});
 module.exports = router;
